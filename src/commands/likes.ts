13bdda8e713cdf497bffafea9092a9db1170b7bf
Camille Louédoc-Eyries <camille.eyries@gmail.com>
2021-11-01 13:09:21 +0100

    feat(cli): command to print all likes of user

---
+++ src/commands/likes.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+/* eslint-disable @typescript-eslint/typedef */
+import { Command } from "@oclif/command";
+import { getLikes } from "../packlets/soundcloud-api";
+
+export default class Likes extends Command {
+  static description =
+    "Get the URLs of all the tracks a user likes on Soundcloud";
+
+  static flags = {};
+
+  static args = [{ name: "userId", required: true }];
+
+  async run() {
+    const { args } = this.parse(Likes);
+
+    this.log("Fetching likes of user ID " + args.userId);
+
+    const likes = await getLikes(args.userId);
+    const tracks = likes.filter((l) => !!l.track);
+
+    this.log("Printing user likes (" + tracks.length + ")");
+    await new Promise<void>((resolve) => setTimeout(resolve, 1000));
+    console.log(tracks.map((t) => t.track!.permalink_url).join("\n"));
+  }
+}
